06671feb2ec62d73c68d68c4e99105c9c0fdb389
Manveer <singh.manveer93@icloud.com>
2020-10-01 10:42:56 -0400

    AddBasicFiles

---
+++ main.py
@@ -0,0 +1,23 @@
+from bottle import route, run, template
+from pykeepass import PyKeePass
+
+# load database
+kp = PyKeePass('ressources/Database.kdbx', password='password')
+
+group = kp.find_groups(name='New Group1', first=True)
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+@route('/')
+def index():
+    return template('index.html', name="Manveer", entries=str(group.entries))
+
+
+run(host='localhost', port=8088, reloader=True)
