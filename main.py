0f163758c7c4ade2509ea74dbf0597831d44736d
Yassine <mp482a@hotmail.fr>
2021-01-31 22:38:19 +0100

    V0.2

--- main.py
+++ main.py
@@ -22,28 +22,27 @@ def callback(path):
 
 
 ################################################################
-#Routage de /login vers la page de connexion
-@route('/login')
-def login():
-    return template('login', loginError = '')
+#Routage de /home vers la page de connexion
+@route('/home')
+def home():
+    return template('home', homeError = '')
 ################################################################
 
 
 ################################################################
-#Routage de /login vers la page de connexion AVEC LA METHODE POST
-@post('/login')
-def do_login():
-    nameDB = request.forms.get('nameDB') #recuperation du nameDB depuis login.html
-    inputPassword = request.forms.get('inputPassword') #recuperation du inputPassword depuis login.html
+#Routage de /home vers la page de connexion AVEC LA METHODE POST
+@post('/home')
+def do_home():
+    nameDB = request.forms.get('nameDB') #recuperation du nameDB depuis home.html
+    inputPassword = request.forms.get('inputPassword') #recuperation du inputPassword depuis home.html
     if not os.path.exists('./ressources/' + str(nameDB)): #cherche si le chemin n'existe pas
-        return template('login', loginError='<div class="alert alert-danger" role="alert">Please enter a correct filename (with extension)</div>') #retour au login.html avec erreur si chemin n'existe pas
+        return template('home', homeError='<div class="alert alert-danger" role="alert">Please enter a correct filename (with extension)</div>') #retour au home.html avec erreur si chemin n'existe pas
     else:
         try:
             global kp
             kp = PyKeePass('./ressources/' + str(nameDB), password=inputPassword) #identification au keepass
             entries=""
             for entry in kp.entries: #pour chaque element (entry) de kp.entries faire:
-                print(str(entry))
                 x = str(entry).split('"', 1)
                 x = str(x[1]).split('(', 1)
                 title = str(x[0]) #split pour récupérer le Title
@@ -51,17 +50,21 @@ def do_login():
                 entries+="<tr><td>" + str(title) + "</td><td>" + str(username[0]) + "</td></tr>" #concaténer ligne à chaque itération
             return template('index', welcomeMsg='<div class="alert alert-success" id="success-alert" role="alert">Welcome to the Datadabase editing interface!<button type="button" class="close" data-dismiss="alert">x</button></div>', dbEntries=entries)
         except (RuntimeError, TypeError, NameError, CredentialsError): #Si erreur lors de l'identification faire:
-            return template('login', loginError='<div class="alert alert-danger" role="alert">Please enter a correct password</div>') #retour au login.html avec erreur si password pas bon
+            return template('home', homeError='<div class="alert alert-danger" role="alert">Please enter a correct password</div>') #retour au home.html avec erreur si password pas bon
 ################################################################
 
-@get('/login/groupmanager')
+@get('/home/groupmanager')
 def groupmanager():
     groups=''
     for group in kp.groups:
         x = str(group).split('"', 1)
-        title = str(x[1])
-        print(title)
-        groups+="<tr><td>" + str(title) + "</td></tr>"
-    return template('groupmanager', dbGroups='groups')
+        x = str(x[1]).split('"', 1)
+        title = str(x[0])
+        groups+="<tr><td><a href='" + str(title) + "'>" + str(title) + "</a></td></tr>"
+    return template('groupmanager', dbGroups=groups)
+
+# @get('/home/<group>')
+# def printGroup():
+#         print('{group}')
 
 run(host='localhost', port=8088, reloader=True, debug=True)
